cdb4ef00ad8d44604b93fb6e4ee19c8730ed3de9
Benji Dial <benji3.141@gmail.com>
2018-06-15 12:05:17 -0400

    $exit$, $set color xx$

--- shell.c
+++ shell.c
@@ -5,18 +5,47 @@
 /*Copyright 2018 Benji Dial
   Portland shell*/
 
-void main(void) {
+void main(uint8_t *arg) {
   uint8_t buffer[1024];
   puts("Welcome to Portland Shell v0.2.0!");
   while (1) {
     putchar('>');
     putchar(' ');
-    if (gets(buffer)[0] == 'e' &&
-              buffer[1] == 'x' &&
-              buffer[2] == 'i' &&
-              buffer[3] == 't' &&
-              buffer[4] == '\0')
+    if (gets(buffer)[0] == '$' &&
+             buffer [1] == 'e' &&
+             buffer [2] == 'x' &&
+             buffer [3] == 'i' &&
+             buffer [4] == 't' &&
+             buffer [5] == '$' &&
+             buffer [6] == '\0') {
+      puts("Goodbye from Portland Shell v0.2.0!");
       return;
-    system(buffer);
+    }
+    if (buffer[ 0] == '$' &&
+        buffer[ 1] == 's' &&
+        buffer[ 2] == 'e' &&
+        buffer[ 3] == 't' &&
+        buffer[ 4] == ' ' &&
+        buffer[ 5] == 'c' &&
+        buffer[ 6] == 'o' &&
+        buffer[ 7] == 'l' &&
+        buffer[ 8] == 'o' &&
+        buffer[ 9] == 'r' &&
+        buffer[10] == ' ' &&
+        buffer[13] == '$' &&
+        buffer[14] == '\0') {
+      uint16_t mask = ((buffer[11] >= 'a' ? 10 + buffer[11] - 'a' :
+                        buffer[11] >= 'A' ? 10 + buffer[11] - 'A' :
+                        buffer[11]  - '0') << 12) |
+                      ((buffer[12] >= 'a' ? 10 + buffer[12] - 'a' :
+                        buffer[12] >= 'A' ? 10 + buffer[12] - 'A' :
+                        buffer[12]  - '0') <<  8);
+      asm volatile (
+        "pushw %mask\n"
+        "int $0x8a\n"
+        "add $2, %sp"
+      );
+    } else
+      system(buffer);
   }
 }
